dc4978bf3d9807ca4914fc92f0936a8024c34b2b
alexandrepradoanalista <ti@ecosystem.net.br>
2024-07-11 14:36:59 -0300

    Agora os dados do pós cálculo não vem mais com linhas duplicadas

--- classes/consulta.py
+++ classes/consulta.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from openpyxl.workbook import Workbook
 
 class Consulta:
     def __init__(self, engine, query, caminho_destino) -> None:
@@ -8,11 +7,14 @@ class Consulta:
         self._query = query
 
     def salvar_arquivo(self):
-        df = pd.read_sql_query(self._query, self._engine)
+        df = pd.read_sql_query(self._query, self._engine).drop_duplicates()
         df.to_excel(self._caminho_destino, index=False)
 
     def testar(self):
         print('teste')
+
+
+        
 
 
 
